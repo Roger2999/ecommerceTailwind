55ef747246cd8b9443524c3573433250d73fc56e
Roger <rogergutierrezmartinez900@gmail.com>
2025-12-26 20:50:14 +0100

    Actualizar QuantityButton.jsx

--- src/components/QuantityButton/QuantityButton.jsx
+++ src/components/QuantityButton/QuantityButton.jsx
@@ -5,7 +5,7 @@ export const QuantityButton = ({ quantity, id, className }) => {
   const lessLot = useCarStore((state) => state.lessLot);
 
   return (
-    <div className="flex flex-col md:flex-row items-center md:gap-2 gap-1 button-container">
+    <div className="flex flex-row items-center md:gap-2 gap-1 button-container">
       <Button handleClick={() => lessLot(id)} label="-" className={className} />
       <span
         className={`text-black font-semibold rounded-lg text-base px-4 py-1 bg-gray-100 border border-gray-300 mx-1 select-none text-center min-w-[2.5rem]`}
